c8fae9f25c2bd5736dded83eaf2b12aa2c5af97f
Karl Heinz Marbaise <info@soebes.de>
2010-12-31 17:30:51 +0100

    - Added new user UserAsterik who represents anybody.

---
+++ src/main/java/com/soebes/subversion/sapm/UserAsterik.java
@@ -0,0 +1,19 @@
+package com.soebes.subversion.sapm;
+
+/**
+ * This user is the user who represents anyone.
+ * 
+ * @author Karl Heinz Marbaise
+ *
+ */
+public class UserAsterik extends User {
+
+	public UserAsterik() {
+		super("*");
+	}
+
+	public boolean isEqual(String user) {
+		return true;
+	}
+	
+}
